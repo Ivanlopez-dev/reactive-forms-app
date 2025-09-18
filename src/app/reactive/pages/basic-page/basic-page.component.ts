d7d1a688cb24477b61f8ee2c1432ffe05e9de968
Ivan López Rodríguez <ivaan100@gmail.com>
2025-09-18 15:25:41 +0200

    add: ✨ showing errors into screen

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -11,16 +11,38 @@ export class BasicPageComponent {
   private fb = inject(FormBuilder);
 
   // ☑️ (FormBuilder) - Readable way
-  myForm = this.fb.group({
+  myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(3)]],
     price: [0, [Validators.required, Validators.min(10)]],
     inStorage: [0, [Validators.required, Validators.min(0)]]
   })
 
-  // ❌ (FormGroup-FormControl) - Not Readable way
-  // myForm2 = new FormGroup({
-  //   name: new FormControl(''),
-  //   price: new FormControl(0),
-  //   inStorage: new FormControl(0),
-  // });
+  isValidField(fieldName: string): boolean | null {
+    // 📝 (!) A single ! means negation (empty), But a
+    //    (!!) A double !! means if there's something
+    // It is often used to check whether something exists or not, instead of returning null or an object.
+    return !!this.myForm.controls[fieldName].errors
+  }
+
+  getFieldError(fieldName: string): string | null {
+
+    if (!this.myForm.controls[fieldName]) return null;
+
+    const errors = this.myForm.controls[fieldName].errors ?? {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido';
+
+        case 'minlength':
+          return `Mínimo de ${errors['minlength'].requiredLength} caracteres.`;
+
+        case 'min':
+          return `Valor mínimo de ${errors['min'].min}.`;
+      }
+    }
+
+    return null;
+  }
 }
